1d9824bf7e8d5853811dc2bc6123d25a8bcd7cd8
dm1tro69 <dimolg22@gmail.com>
2021-12-02 23:11:49 +0200

    add test alert

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,18 +3,24 @@ import axios from "axios";
 import ScoopOption from "./ScoopOption";
 import {Row} from "react-bootstrap";
 import ToppingOptions from "./ToppingOptions";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({optionType}) => {
 
     const [items, setItems] = useState([])
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         axios.get(`http://localhost:3030/${optionType}`)
             .then(response => setItems(response.data))
             .catch(err => {
-
+                setError(true)
             })
     }, [optionType])
+
+    if (error) {
+        return <AlertBanner/>
+    }
 
     const ItemComponent = optionType === 'scoops'? ScoopOption : ToppingOptions
     const optionItems = items.map((item) => {
