65dc4ad840f2b496f531e03d62b5a689062b4eb8
dm1tro69 <dimolg22@gmail.com>
2021-12-03 21:36:18 +0200

    add context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import SummaryForm from "./pages/summary/SummaryForm";
 import Options from "./pages/entry/Options";
+import {Container} from "react-bootstrap";
+import OrderEntry from "./pages/entry/OrderEntry";
+import {OrderDetailsProvider} from "./context/OrderDetails";
 
 function App() {
   return (
-    <div>
-     <SummaryForm/><Options optionType={'scoops'}/>
-     <SummaryForm/><Options optionType={'toppings'}/>
-    </div>
+    <Container>
+        <OrderDetailsProvider>
+            <SummaryForm/><Options optionType={'scoops'}/>
+            <SummaryForm/><Options optionType={'toppings'}/>
+            <OrderEntry/>
+        </OrderDetailsProvider>
+
+
+    </Container>
   );
 }
 
--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,11 +4,15 @@ import ScoopOption from "./ScoopOption";
 import {Row} from "react-bootstrap";
 import ToppingOptions from "./ToppingOptions";
 import AlertBanner from "../common/AlertBanner";
+import {pricePerItem} from "../../constans";
+import {useOrderDetails} from "../../context/OrderDetails";
 
 const Options = ({optionType}) => {
 
     const [items, setItems] = useState([])
     const [error, setError] = useState(false)
+
+    const [orderDetails, updateItemCount] = useOrderDetails()
 
     useEffect(() => {
         axios.get(`http://localhost:3030/${optionType}`)
@@ -23,16 +27,28 @@ const Options = ({optionType}) => {
     }
 
     const ItemComponent = optionType === 'scoops'? ScoopOption : ToppingOptions
+
+    const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase()
+
     const optionItems = items.map((item) => {
         return (
-            <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath}/>
+            <ItemComponent
+                key={item.name}
+                name={item.name}
+                imagePath={item.imagePath}
+                updateItemCount={(itemName, newItemCount) => updateItemCount(itemName, newItemCount, optionType)}/>
         )
     })
 
     return (
+        <>
+            {title}
+            <p>{pricePerItem[optionType]} each</p>
+            <p>{title} total: {orderDetails.totals[optionType]}</p>
         <Row>
             {optionItems}
         </Row>
+        </>
     );
 };
 
